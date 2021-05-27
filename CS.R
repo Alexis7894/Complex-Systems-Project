a49ad5e6185e56d41fbcc673e1b2145ede7d0eae
Alexis7894 <65927248+Alexis7894@users.noreply.github.com>
2021-05-27 15:49:58 +0200

    Add files via upload

--- CS.R
+++ CS.R
@@ -225,35 +225,6 @@ crqa_PeC5TP7 = crqa(ts1 = PeC5, ts2 = PeTP7, delay = 9,
 
 
 
-
-
-
-
-
-
-
-
-
-crqa_PeC5F1 = crqa(ts1 = PeC5, ts2 = PeF1, delay = 9,
-                   embed = 3, rescale = 2, radius = 20, normalize = 2, mindiagline = 2,
-                   minvertline = 2, tw = 0, whiteline = FALSE, recpt = FALSE,
-                   side = "both") 
-
-crqa_PeC5F3 = crqa(ts1 = PeC5, ts2 = PeF3, delay = 9,
-                   embed = 3, rescale = 2, radius = 20, normalize = 2, mindiagline = 2,
-                   minvertline = 2, tw = 0, whiteline = FALSE, recpt = FALSE,
-                   side = "both") 
-
-crqa_PeC5F5 = crqa(ts1 = PeC5, ts2 = PeF5, delay = 9,
-                   embed = 3, rescale = 2, radius = 20, normalize = 2, mindiagline = 2,
-                   minvertline = 2, tw = 0, whiteline = FALSE, recpt = FALSE,
-                   side = "both") 
-
-
-
-
-
-
 crqa_PeF1T7 = crqa(ts1 = PeF1, ts2 = PeT7, delay = 9,
                    embed = 3, rescale = 2, radius = 20, normalize = 2, mindiagline = 2,
                    minvertline = 2, tw = 0, whiteline = FALSE, recpt = FALSE,
@@ -308,3 +279,105 @@ crqa_PeF1TP7 = crqa(ts1 = PeF5, ts2 = PeTP7, delay = 9,
 
 
 
+#Prepare MdCRQA
+
+
+data = cbind(C1, C3, C5, F1,F3,F5,T7,FT7,TP7)
+emb =
+del =
+norm =
+rad =
+
+if (exists("data")) {
+} else {
+  print("No data has been specified")
+}
+
+if (exists("emb")) {
+} else {
+  emb <- 1
+}
+
+if (exists("del")) {
+} else {
+  del <- 1
+}
+
+if (exists("norm")) {
+} else {
+  norm <- "non"
+}
+
+if (exists("rad")) {
+} else {
+  rad <- 1
+}
+
+# compute and store parameter settings
+dims <- dim(data)[2]
+PARAMETERS <- c(dims, emb, del, rad, norm)
+
+# cut data to dims and check whether embedding needs to be performed
+data <- data[,1:dims]
+if (emb > 1) {
+  newLength <- dim(data)[1] - (emb-1)*del
+  tempdata <- data[1:newLength,]
+  for (i in seq(2,emb)) {
+    tempdata <- cbind(tempdata,data[(1+(del*(i-1))):(newLength+del*(i-1)),])
+  }
+  data <- tempdata
+  rm(tempdata)
+}
+
+# create distance matrix
+RP <- as.matrix(abs(dist(data, diag = T, upper = T))*-1)
+
+# apply norm, radius and threshold matrix
+if (norm == "euc") {
+  RP <- RP/abs(sum(RP)/(dim(RP)[1]^2-dim(RP)[1]))
+} else if (norm == "min") {
+  RP <- RP/abs(min(RP))
+} else if (norm == "max") {
+  RP <- RP/abs(max(RP))
+} else {
+}
+
+RP <- RP + rad
+RP <- ifelse(RP < 0, 0, 1)
+
+# calculate diagonal and vertical line distributions
+diag(RP) <- 0
+diagLine <- split(RP,row(RP) - col(RP))
+diagHist <- 0
+for (i in seq(1,(dim(RP)[1]-1)*2)) {
+  diagHist <- append(diagHist,tabulate(ConnCompLabel(as.numeric(unlist(diagLine[i])))))
+}
+diagHist <- diagHist[!diagHist %in% 0]
+
+diag(RP) <- 1
+vertLine <- split(RP,col(RP))
+vertHist <- 0
+for (i in seq(1,(dim(RP)[1]-1)*2)) {
+  vertHist <- append(vertHist,tabulate(ConnCompLabel(as.numeric(unlist(vertLine[i])))))
+}
+vertHist <- vertHist[!vertHist %in% 0]
+
+# calculate MdRQA outplut
+diag(RP) <- 0
+RESULTS <- dim(RP)[1] # size of RP
+RESULTS[2] <- 100*sum(RP)/(dim(RP)[1]^2 - dim(RP)[1]) # percent recurrence
+RESULTS[3] <- 100*sum(diagHist[!diagHist %in% 1])/sum(diagHist) # percent determinism
+RESULTS[4] <- mean(diagHist[!diagHist %in% 1]) # average diagonal line
+RESULTS[5] <- max(diagHist) # maximum diagonal line
+RESULTS[6] <- entropy(diagHist[!diagHist %in% 1]) # diagonal line entropy
+RESULTS[7] <- 100*sum(vertHist[!vertHist %in% 1])/sum(vertHist) # percent laminarity
+RESULTS[8] <- mean(vertHist[!vertHist %in% 1]) # average vertical line
+RESULTS[9] <- max(vertHist) # maximum vertical line
+RESULTS[10] <- entropy(vertHist[!vertHist %in% 1]) # vertical line entropy
+diag(RP) <- 1
+RP = Matrix(RP, sparse = TRUE)
+
+output <- list(SizeRP = RESULTS[1], REC = RESULTS[2], DET = RESULTS[3], ADL = RESULTS[4], MDL = RESULTS[5], DENTR = RESULTS[6], LAM = RESULTS[7], AVL = RESULTS[8], MVL = RESULTS[9], VENTR = RESULTS[10], DIM = PARAMETERS[1], EMB = PARAMETERS[2], DEL = PARAMETERS[3], RAD = PARAMETERS[4], NORM = PARAMETERS[5], RP = RP)
+
+return(output)
+}
